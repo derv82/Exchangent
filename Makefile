9c4a89a60e8c6ed010c4d90082a38dd309a2829e
derv82 <derv82@gmail.com>
2016-09-05 15:09:19 -0700

    Preferences hide/show appropriately.
    Holy crap preferences are hard.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ include $(THEOS)/makefiles/common.mk
 ARCHS = armv7 armv7s arm64
 
 TWEAK_NAME = Exchangent
-Exchangent_FRAMEWORKS = UIKit
+#Exchangent_FRAMEWORKS = UIKit
 Exchangent_FILES = Tweak.xm
 
 include $(THEOS_MAKE_PATH)/tweak.mk
--- exchangentprefs/ExchangentPrefsRootListController.m
+++ exchangentprefs/ExchangentPrefsRootListController.m
@@ -4,13 +4,62 @@
 #define nsPreferencesPath @"/User/Library/Preferences/com.derv82.exchangentprefs.plist"
 
 @interface ExchangentPrefsRootListController : PSListController {
+  BOOL shouldReload;
 }
 @end
 
 @implementation ExchangentPrefsRootListController
 -(id) specifiers {
+  if (shouldReload) {
+    shouldReload = NO;
+    [_specifiers release];
+    _specifiers = nil;
+  }
 	if (!_specifiers) {
      NSMutableArray *specifiers = [[self loadSpecifiersFromPlistName:@"ExchangentPrefs" target:self] mutableCopy];
+     NSDictionary *exchangentSettings = [NSDictionary dictionaryWithContentsOfFile:nsPreferencesPath];
+     id _tempValue;
+     
+     _tempValue = [exchangentSettings objectForKey:@"enabled"];
+     BOOL isTweakEnabled = _tempValue ? [_tempValue boolValue] : NO;
+
+     _tempValue = [exchangentSettings objectForKey:@"useCustom"];
+     BOOL isCustomEnabled = _tempValue ? [_tempValue boolValue] : NO;
+
+     NSMutableArray *specifiersToRemove = [[NSMutableArray alloc] init];
+     for (PSSpecifier *specifier in specifiers) {
+       NSLog(@"Specifier identifier: %@", specifier.identifier);
+       if (isCustomEnabled || !isTweakEnabled) {
+         if ([specifier.identifier isEqualToString:@"User-Agent for Device and Version"]) {
+           [specifiersToRemove addObject:specifier];
+         } else if ([specifier.identifier isEqualToString:@"Preset Versions"]) {
+           [specifiersToRemove addObject:specifier];
+         } else if ([specifier.identifier isEqualToString:@"Device"]) {
+           [specifiersToRemove addObject:specifier];
+         } else if ([specifier.identifier isEqualToString:@"iOS Version"]) {
+           [specifiersToRemove addObject:specifier];
+         } else if ([specifier.identifier isEqualToString:@"Preset User-Agent"]) {
+           [specifiersToRemove addObject:specifier];
+         }
+       }
+       if (!isCustomEnabled || !isTweakEnabled) {
+         if ([specifier.identifier isEqualToString:@"User-Agent:"]) {
+           [specifiersToRemove addObject:specifier];
+         }
+       }
+       if (!isTweakEnabled) {
+         if ([specifier.identifier isEqualToString:@"Custom"]) {
+           [specifiersToRemove addObject:specifier];
+         } else if ([specifier.identifier isEqualToString:@"Use Custom User-Agent"]) {
+           [specifiersToRemove addObject:specifier];
+         }
+       }
+     }
+
+     NSLog(@"Specifiers to remove: %@", specifiersToRemove);
+     for (PSSpecifier *specifierToRemove in specifiersToRemove) {
+       [specifiers removeObject:specifierToRemove];
+     }
      _specifiers = [specifiers copy];
      [specifiers release];
 	}
@@ -19,22 +68,37 @@
 }
 
 -(id) readPreferenceValue:(PSSpecifier*)specifier {
-    NSDictionary *exchangentSettings = [NSDictionary dictionaryWithContentsOfFile:nsPreferencesPath];
-    if (!exchangentSettings[specifier.properties[@"key"]]) {
-        return specifier.properties[@"default"];
-    }
-    return exchangentSettings[specifier.properties[@"key"]];
+  id result;
+  NSDictionary *exchangentSettings = [NSDictionary dictionaryWithContentsOfFile:nsPreferencesPath];
+  NSString *key = specifier.properties[@"key"];
+  if ([key isEqualToString:@"presetUserAgent"]) {
+    result = [NSString stringWithFormat:@"%@/%@", exchangentSettings[@"device"], exchangentSettings[@"iosVersion"]];
+  } else if (!exchangentSettings[specifier.properties[@"key"]]) {
+    result = specifier.properties[@"default"];
+  } else {
+    result = exchangentSettings[specifier.properties[@"key"]];
+  }
+
+  if ([key isEqualToString:@"device"] || [key isEqualToString:@"iosVersion"]) {
+    [self reloadSpecifierID:@"Preset User-Agent"];
+  }
+  return result;
 }
 
 -(void) setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
-    NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
-    [defaults addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:nsPreferencesPath]];
-    [defaults setObject:value forKey:specifier.properties[@"key"]];
-    [defaults writeToFile:nsPreferencesPath atomically:YES];
-    CFStringRef toPost = (CFStringRef)specifier.properties[@"PostNotification"];
-    if (toPost) {
-      CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), toPost, NULL, NULL, YES);
-    }
+  NSMutableDictionary *defaults = [NSMutableDictionary dictionary];
+  [defaults addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:nsPreferencesPath]];
+  [defaults setObject:value forKey:specifier.properties[@"key"]];
+  [defaults writeToFile:nsPreferencesPath atomically:YES];
+  CFStringRef toPost = (CFStringRef)specifier.properties[@"PostNotification"];
+  if (toPost) {
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), toPost, NULL, NULL, YES);
+  }
+  if ([specifier.properties[@"key"] isEqualToString:@"useCustom"] || [specifier.properties[@"key"] isEqualToString:@"enabled"]) {
+    NSLog(@"Reloading specifiers");
+    shouldReload = YES;
+    [self reloadSpecifiers];
+  }
 }
 
 @end
